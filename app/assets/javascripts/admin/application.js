985832b80535ad5282f446378b1d4d523b7f89cd
leminhy89 <leminhy89@gmail.com>
2017-02-08 09:21:54 +0000

    First init

---
+++ app/assets/javascripts/admin/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require admin/jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require admin/bootstrap
+//= require admin/fastclick
+//= require admin/nprogress
+//= require admin/chart
+//= require admin/gauge
+//= require admin/bootstrap-progressbar
+//= require admin/icheck
+//= require admin/skycons
+//= require admin/jqueryflot
+//= require admin/jqueryflotpie
+//= require admin/jqueryflottime
+//= require admin/jqueryflotstack
+//= require admin/jqueryflotresize
+//= require admin/jqueryflotorderBars
+//= require admin/jqueryflotspline
+//= require admin/curvedLines
+//= require admin/date
+//= require admin/jqueryvmap
+//= require admin/jqueryvmaporld
+//= require admin/jqueryvmapsampledata
+//= require admin/moment
+//= require admin/daterangepicker
+//= require admin/custom
+//= require_tree .
